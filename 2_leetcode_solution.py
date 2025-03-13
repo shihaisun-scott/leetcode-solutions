cf5ab59b49f9193c9a51d052101e72ed9d839327
shihaisun-scott <shihaisun.scott@gmail.com>
2025-03-13 13:12:00 -0400

    Added Two Sum solution

---
+++ 2_leetcode_solution.py
@@ -0,0 +1,17 @@
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val  # The value stored in the node
+        self.next = next  # Reference to the next node in the list
+
+
+l1 = [2,4,3]
+l2 = [5,6,4]
+
+ListNode(1)
+
+for i1 in range(len(nums)):
+            for i2 in range(len(nums)):
+                if i1 != i2:
+                    if nums[i1] + nums[i2] == target:
+                        print[i1, i2]
