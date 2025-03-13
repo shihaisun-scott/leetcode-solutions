c830c37eb7cad8aeb7c3667ede9bcecb571b2d51
shihaisun-scott <shihaisun.scott@gmail.com>
2025-03-13 13:05:02 -0400

    added two sum

---
+++ 1_twosum.py
@@ -0,0 +1,52 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# You can return the answer in any order.
+
+# Example 1:
+
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+# Example 2:
+
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+# Example 3:
+
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+ 
+
+# Constraints:
+
+# 2 <= nums.length <= 104
+# -109 <= nums[i] <= 109
+# -109 <= target <= 109
+# Only one valid answer exists.
+
+
+# nums = [3,2,4]
+# target = 6
+# nums = [3,3]
+# target = 6
+
+def twoSum(nums: list[int], target: int) -> list[int]:
+    for i, num1 in enumerate(nums):
+        for ii, num2 in enumerate(nums):
+            if i != ii:
+                x = num1 + num2
+                if x == target:
+                    return(i, ii)
+
+# Local Testing
+nums = [2, 7, 11, 15]
+target = 9
+print(twoSum(nums, target))  # Expected Output: [0, 1]
+
+
+
+
+
+
